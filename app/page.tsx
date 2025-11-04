b58ad33df42241367078567cea99e2b0e7fc8e66
noel <noel.merz09@outlook.com>
2025-11-04 12:52:07 +0100

    wip page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,17 @@
+import { TriangleAlert } from "lucide-react";
+
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-
+    <div className="flex min-h-screen flex-col items-center justify-center  text-center">
+      <main className="p-6 rounded-2xl bg-gray-100 ">
+        <center>
+          <TriangleAlert className="mb-4 h-16 w-16 " />
+        </center>
+        <h1 className="text-2xl font-semibold mb-2 text-gray-800">
+          Work in Progress.
+        </h1>
+        <p className="text-gray-600">
+        </p>
       </main>
     </div>
   );
