ca2038a220746555b80dd395028998c92f1d7539
noqx <noel.merz09@outlook.com>
2025-11-04 23:08:58 +0100

    darkmode

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,24 @@
 import { TriangleAlert } from "lucide-react";
-import { Separator } from "@/components/ui/separator"
+import { Separator } from "@/components/ui/separator";
+import { ThemeSwitcher } from "@/components/theme-switcher";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center  text-center">
-      <main className="p-6 rounded-2xl bg-gray-100 ">
+    <div className="flex min-h-screen flex-col items-center justify-center text-center bg-background text-foreground">
+      <main className="p-6 rounded-2xl bg-card text-card-foreground shadow">
         <center>
           <TriangleAlert className="mb-4 h-16 w-16 " />
         </center>
-        <h1 className="text-2xl font-semibold mb-2 text-gray-800">
-          Work in Progress.
-        </h1>
-        <p className="text-gray-950 ">
-          <Separator className="my-4" />
-          <p className="mb-2">page list</p>
-      <div className="flex flex-col text-gray-600">
-        <a href="register">/register</a>
-        <a href="login">/login</a>
-      </div>
+        <h1 className="text-2xl font-semibold mb-2">Work in Progress.</h1>
+        <Separator className="my-4" />
+        <ThemeSwitcher />
+        <Separator className="my-4" />
 
-          </p>
+        <p className="mb-2">page list</p>
+        <div className="flex flex-col text-muted-foreground gap-1">
+          <a href="register" className="hover:underline">/register</a>
+          <a href="login" className="hover:underline">/login</a>
+        </div>
       </main>
     </div>
   );
