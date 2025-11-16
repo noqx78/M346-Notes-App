7248ad029842a2dc8ba121f4fe4e4a5e42253709
noqx <noel.merz09@outlook.com>
2025-11-16 14:46:07 +0100

    wip: /notes ui changes

--- components/NotesNavigation.tsx
+++ components/NotesNavigation.tsx
@@ -5,6 +5,8 @@ import { useAuth } from "@/hooks/useAuth";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { Button } from "./ui/button";
 import { Separator } from "@radix-ui/react-separator";
+import { Input } from "./ui/input";
+
 {/* 
     Todo:
     -whitemode interg
@@ -17,25 +19,36 @@ export default function NotesNavigation() {
 
   return (
     <nav className="flex flex-col p-6 gap-4 w-64 min-w-[16rem] min-h-screen  text-muted-foreground">
-      <div className="flex flex-col items-center gap-2 ">
-        <Avatar>
-          <AvatarImage src="" alt="User" />
-          <AvatarFallback>JD</AvatarFallback>
-        </Avatar>
-        <p className="text-white text-sm">{user?.email}</p>
+      <h1>Notes</h1>
+
+      <Separator className="my-4 h-px bg-border" />
+      <Input type="text" placeholder="Search for Notes" />
+      <Button variant="outline">
+        Create new Note
+      </Button>
+
+
+      <div className="flex flex-col  place-items-stretch gap-1 ">
+        <Button asChild variant="ghost">
+          <Link href="/notes/1">Note 1</Link>
+        </Button>
+        <Button asChild variant="ghost">
+          <Link href="/notes/2">Note 2</Link>
+        </Button>
       </div>
 
       <Separator className="my-4 h-px bg-border" />
+      <Button variant="outline">
+        Settings
+      </Button>
 
-      <Button asChild variant="ghost">
-        <Link href="/notes/1">Button 1</Link>
-      </Button>
-      <Button asChild variant="ghost">
-        <Link href="/notes/2">Button 2</Link>
-      </Button>
-      <Button asChild variant="ghost">
-        <Link href="/notes/3">Button 3</Link>
-      </Button>
+<Button variant="ghost">
+      <Avatar>
+        <AvatarImage src="" alt="User" />
+        <AvatarFallback>JD</AvatarFallback>
+      </Avatar>
+      <p className="text-foreground text-sm">{user?.email}</p>
+</Button>
 
     </nav>
   );
